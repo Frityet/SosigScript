877fa9edcd0fc2a1a550ac73cd270c4e756f6eb2
Frityet <ambhogal01@gmail.com>
2021-09-08 07:46:23 -0700

    Started adding types

--- SosigScript.Core/Libraries/SosigScriptLibrary.cs
+++ SosigScript.Core/Libraries/SosigScriptLibrary.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Reflection;
+using BepInEx;
+using MoonSharp.Interpreter;
 using SosigScript.Resources;
 
 namespace SosigScript.Libraries
@@ -8,16 +11,19 @@ namespace SosigScript.Libraries
 	{
 		public ResourceMetadata LibraryInfo { get; }
 		public IDictionary<string, object> LoadedObjects => _loadedObjects;
+		public IDictionary<string, Type> LoadedTypes => _loadedTypes;
 		public Assembly Assembly { get; }
 
 		internal LibraryLoader? LibraryLoader { get; set; }
 
+		private Dictionary<string, Type> _loadedTypes;
 		private Dictionary<string, object> _loadedObjects;
 
 		public SosigScriptLibrary(ResourceMetadata libraryInfo)
 		{
 			LibraryInfo = libraryInfo;
 			_loadedObjects = new Dictionary<string, object>();
+			_loadedTypes = new Dictionary<string, Type>();
 			Assembly = Assembly.GetCallingAssembly();
 
 			LibraryLoader.AddLibrary(this);
@@ -27,9 +33,13 @@ namespace SosigScript.Libraries
 		{
 			_loadedObjects.Add(name, obj);
 
-
-
 			Plugin.ScriptLoader!.AddObject(name, obj);
+		}
+
+		public void RegisterType(Type type, string name = "")
+		{
+			UserData.RegisterType(type, InteropAccessMode.Default, name.IsNullOrWhiteSpace() || String.IsNullOrEmpty(name) ? null : name);
+			_loadedTypes.Add(name.IsNullOrWhiteSpace() || String.IsNullOrEmpty(name) ? type.Name : name, type);
 		}
 	}
 }
---
+++ SosigScript.Core/Libraries/SosigScriptTypeAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace SosigScript.Libraries
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+	public class SosigScriptTypeAttribute : Attribute
+	{
+
+	}
+}
